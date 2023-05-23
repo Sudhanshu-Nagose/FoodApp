13799e5cf6f4b07785fbe214ef615cb3175ed9ea
Sudhanshu-Nagose <sudhanshunagose121@gmail.com>
2023-05-23 23:18:24 +0530

    change

--- scr/screens/Home.js
+++ scr/screens/Home.js
@@ -342,7 +342,7 @@ const Home = ({ navigation }) => {
                   style={{ color: textColor, fontSize: 18, fontWeight: "500" }}>
                   {item.name}
                 </Text>
-                <Text style={{ fontSize: 14, marginTop: hp(0.5), color:text }}>
+                <Text style={{ fontSize: 14, marginTop: hp(0.5), color: text }}>
                   {item.desc}
                 </Text>
 
@@ -396,7 +396,7 @@ const Home = ({ navigation }) => {
           <Text style={{ fontWeight: "bold", fontSize: 17, color: textColor }}>
             Popular Meal Menu
           </Text>
-          <Text style={{color:text}}>See All</Text>
+          <Text style={{ color: text }}>See All</Text>
         </View>
 
         {popularMeals.map((item, index) => {
@@ -424,7 +424,9 @@ const Home = ({ navigation }) => {
                   style={{ fontWeight: "500", fontSize: 16, color: textColor }}>
                   {item.name}
                 </Text>
-                <Text style={{ marginTop: hp(0.5), color:text }}>{item.desc}</Text>
+                <Text style={{ marginTop: hp(0.5), color: text }}>
+                  {item.desc}
+                </Text>
               </View>
               <Text
                 style={{
@@ -459,7 +461,7 @@ const Home = ({ navigation }) => {
         <TextInput
           placeholder="Search"
           placeholderTextColor="#808080"
-          style={{ fontSize: 17, fontWeight: "500", color:textColor }}
+          style={{ fontSize: 17, fontWeight: "500", color: textColor }}
         />
       </View>
     );
@@ -495,7 +497,7 @@ const Home = ({ navigation }) => {
             resizeMode="stretch"
             style={{ width: wp(3.5), height: wp(5), marginRight: wp(2) }}
           />
-          <Text style={{color:text}}>Freedom way, Lekki phase</Text>
+          <Text style={{ color: text }}>Freedom way, Lekki phase</Text>
         </View>
         <Image
           source={require("../images/profile_icon.png")}
